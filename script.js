af5e667675c47f3d2d523dca26a3267645c08219
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2022-11-07 21:53:56 +0000

    started implementing game elements

--- script.js
+++ script.js
@@ -81,14 +81,36 @@ const toKeyPress = (x) => {
 // as querySelector always finds the first thing it matches, .tile will keep overriding the first tile
 // it allows us to letter through the grid while playing
 const keyPress = (key) => {
+  const tilesInPlay = getTilesInPlay();
+  if (tilesInPlay.length >= 5) return; // more than 5 is 'out of play'
   const nextTile = guessGrid.querySelector(":not([data-letter])");
   nextTile.dataset.letter = key.toUpperCase(); // i think this is matching the dictionary
   nextTile.textContent = key; // sets the text equal to the key
-  nextTile.dataset.state = "active"; // gives the bg colour
+  nextTile.dataset.state = "active"; // gives the 'active' brighter tile indication
 };
 
+// with the keypress function, this ensures our submission is kept to those 5 tiles
+// this function will be called in most game functionality as it centres our core data
+const getTilesInPlay = () => {
+  return guessGrid.querySelectorAll('[data-state="active"]');
+};
 
+// delete functionality
+// finds the tiles in play, get the last tile that was active
+// removes all data connected with that tile
+const backspace = () => {
+  const tilesInPlay = getTilesInPlay();
+  const previousTile = tilesInPlay[tilesInPlay.length - 1]; // this gives us the last tile
+  if (previousTile === null) return; // if there is nothing to delete, we don't need the function
+  previousTile.textContent = ""; // sets it back to empty
+  delete previousTile.dataset.state; // removes the state completely
+  delete previousTile.dataset.letter; // removes the connection to that letter state
+};
 
+const takeGuess = () => {
+
+}
+/////////////////////////////
 
 // this function can't be called before initialisation? so it's starting the game here
 startGame();
@@ -96,3 +118,4 @@ startGame();
 // game was not responding because I hadn't logged the press to the console. It is now appearing
 // for some reason startGame() refuses to be called before.
 // it would help if i called my keyPress / keyPress function correctly
+// delete button isn't working
